edb96463a4f66860c495615f166f9a463e316d72
omeryttnc <omeryttnc@gmail.com>
2023-07-18 19:29:08 +0100

    first commit

---
+++ src/test/java/pages/HomePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class HomePage extends CommonPage {
+
+}
---
+++ src/test/java/utilities/ApiUtilities.java
@@ -0,0 +1,12 @@
+package utilities;
+
+
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+public class ApiUtilities {
+    static ResponseSpecification responseSpecification;
+    static RequestSpecification requestSpecification;
+
+}
+
