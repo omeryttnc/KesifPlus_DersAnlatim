246042036d31f2dfda05c4feb5055d25234bdaf7
omeryttnc <omeryttnc@gmail.com>
2023-08-01 21:25:57 +0100

    links ler calisiyor mu enum class karsilastirmasi extension methodlar genel feature bilgilendirmesi

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -3,8 +3,7 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 public class HomePage extends CommonPage {
